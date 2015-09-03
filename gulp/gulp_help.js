e393c9e6974079522b3bcb537558aa09d77cd646
v-tkalek <v-tkalek@microsoft.com>
2015-09-03 14:04:45 -0700

    add all parameters description

--- gulp/gulp_help.js
+++ gulp/gulp_help.js
@@ -28,15 +28,31 @@ var gulp = require('gulp');
 gulp.task('help', function (callback) {
 
     console.log("\nPowerBI-visuals.\n");
+    console.log("Commands usage: gulp <name_of_command> --<parameter>.\nFor example: gulp build --debug.\n");
     console.log("List of Gulp commands:\n");
     console.log("	build - to build the project. All js files will be minified.");
     console.log("	build_debug - build the project in debug mode.\n 			All js files will be unminified");
     console.log("	tslint - check the source files for tsLint erros.");
     console.log("	test - run tests.");
-    console.log("	run_performance_tests - run only performance tests.\n\n");
-
-    console.log("Gulp commands parameters:\n\n 	--debug  - to build files in debug mode (unminified)\n\n");
-
-    console.log("Commands usage: gulp <name_of_command> --<parameter>.\nFor example: gulp build --debug.\n");
-
+    console.log("	run:performance_tests - run only performance tests.\n\n");
+    console.log("	continuous_build - run the build and then start watchers which\n"+
+    "				will build project parts after you save\n"+
+    "				changes to any file .");
+    console.log("Gulp commands parameters:\n\n 	--debug (--d)  - to build files in debug mode\n\n"+
+				" 	--openInBrowser (--o)  - in order to run all tests\n 				 (or single test) in browser\n"+
+				" 		For example:\n"+
+				"			gulp test --openInBrowser\" or \"gulp test --o\"\n"+
+				"			- runs all tests;\n"+
+				" 			gulp test --openInBrowser testName\" or \n"+
+				"			\"gulp test --o testName\" - runs test\n"+
+				"			\"testName\" in browser;\n\n"+
+				"	--files (--f) 	-  is used to run tests in some .ts file\n\n"+
+				" 		For example:\n"+
+				"			\"gulp test --files testFileName\" or\n"+
+				"			\"gulp test --f testFileName\" - runs tests that contains\n"+
+				"			\"testFileName.ts\" file\n"+
+				" 			\"gulp test --files testFileName --o\" or \n"+
+				" 			\"gulp test --f testFileName --o\" - also it can be\n"+
+				" 			used with \"--o\" option and in this case it runs all\n"+
+				"			tests in testFileName.ts in browser\n");
 });

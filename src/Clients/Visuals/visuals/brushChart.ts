e44e8d0a460a530647628387830f32b24d7a11e1
Hussain <husskash@gmail.com>
2015-11-11 11:31:05 +0100

    strict control

--- src/Clients/Visuals/visuals/brushChart.ts
+++ src/Clients/Visuals/visuals/brushChart.ts
@@ -110,29 +110,31 @@ module powerbi.visuals {
         public static converter(dataView: DataView): BrushChartViewModel {
 
             var catDv: DataViewCategorical = dataView.categorical;
+            if (catDv.categories && catDv.values) {
+                var cat = catDv.categories[0];
+                var catValues = cat.values;
+                var values = catDv.values;
+                var dataPoints: BrushChartData[] = [];
 
-            var cat = catDv.categories[0];
-            var catValues = cat.values;
-            var values = catDv.values;
-            var dataPoints: BrushChartData[] = [];
-
-            for (var i = 0, len = catValues.length; i < len; i++) {
-                if ((catValues[i] instanceof Date) && !isNaN(values[0].values[i])) {
-                    dataPoints.push({
-                        x: catValues[i],
-                        y: values[0].values[i]
-                    });
+                for (var i = 0, len = catValues.length; i < len; i++) {
+                    if ((catValues[i] instanceof Date) && !isNaN(values[0].values[i])) {
+                        dataPoints.push({
+                            x: catValues[i],
+                            y: values[0].values[i]
+                        });
+                    }
                 }
+
+                var viewModel: BrushChartViewModel = {
+                    points: dataPoints,
+                    xLabel: cat.source.displayName,
+                    yLabel: values[0].source.displayName,
+                    yFormat: values[0].source.format
+                };
+
+                return viewModel;
             }
-
-            var viewModel: BrushChartViewModel = {
-                points: dataPoints,
-                xLabel: cat.source.displayName,
-                yLabel: values[0].source.displayName,
-                yFormat: values[0].source.format
-            };
-
-            return viewModel;
+            
         }
 
         private svg: D3.Selection;
@@ -189,46 +191,50 @@ module powerbi.visuals {
             var warnings: IVisualWarning[] = [];
                                     
             // category field is not datetime
-            if (!dataView.categorical.categories[0].source.type.dateTime) {
-                warnings.push({
-                    code: 'NotDateCategoryField',
+            if (dataView.categorical.categories) {
+                if (!dataView.categorical.categories[0].source.type.dateTime) {
+                    warnings.push({
+                        code: 'NotDateCategoryField',
 
-                    getMessages(resourceProvider: any): IVisualErrorMessage {
+                        getMessages(resourceProvider: any): IVisualErrorMessage {
 
-                        var visualMessage: IVisualErrorMessage = {
-                            message: BrushChart.category_error_message,
-                            title: BrushChart.category_error_title,
-                            detail: BrushChart.category_error_message,
-                        };
+                            var visualMessage: IVisualErrorMessage = {
+                                message: BrushChart.category_error_message,
+                                title: BrushChart.category_error_title,
+                                detail: BrushChart.category_error_message,
+                            };
 
-                        return visualMessage;
-                    }
-                });
-                this.host.setWarnings(warnings);
-                this.generateError(1, viewport);
+                            return visualMessage;
+                        }
+                    });
+                    this.host.setWarnings(warnings);
+                    this.generateError(1, viewport);
+                }
             }
+            if (dataView.categorical.values){
+                // Y is not numeric
+                if (!dataView.categorical.values[0].source.type.numeric) {
+                    warnings.push({
+                        code: 'NotNumericYAxisField',
 
-            // Y is not numeric
-            else if (!dataView.categorical.values[0].source.type.numeric) {
-                warnings.push({
-                    code: 'NotNumericYAxisField',
+                        getMessages(resourceProvider: any): IVisualErrorMessage {
 
-                    getMessages(resourceProvider: any): IVisualErrorMessage {
+                            var visualMessage: IVisualErrorMessage = {
+                                message: BrushChart.yaxis_error_message,
+                                title: BrushChart.yaxis_error_title,
+                                detail: BrushChart.yaxis_error_message,
+                            };
 
-                        var visualMessage: IVisualErrorMessage = {
-                            message: BrushChart.yaxis_error_message,
-                            title: BrushChart.yaxis_error_title,
-                            detail: BrushChart.yaxis_error_message,
-                        };
-
-                        return visualMessage;
-                    }
-                });
-                this.host.setWarnings(warnings);
-                this.generateError(2, viewport);
-            } else {
-                this.cleanError();
+                            return visualMessage;
+                        }
+                    });
+                    this.host.setWarnings(warnings);
+                    this.generateError(2, viewport);
+                } else {
+                    this.cleanError();
+                }
             }
+           
         }
 
         // add error message
@@ -291,169 +297,170 @@ module powerbi.visuals {
             this.validateAxis(this.dataView, this.host, viewport);
 
             viewModel = BrushChart.converter(this.dataView);
-            
-            var data = viewModel.points;
-
-            // apply visual style and set functionalities
-            var x = d3.time.scale().range([0, width]),
-                x2 = d3.time.scale().range([0, width]),
-                y = d3.scale.linear().range([height, 0]),
-                y2 = d3.scale.linear().range([height2, 0]);
-
-            var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-                xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
-                yAxis = d3.svg.axis().scale(y).orient("left");
-
-            var area = d3.svg.area()
-                .interpolate("monotone")
-                .x(function (d) { return x(d.x); })
-                .y0(height)
-                .y1(function (d) { return y(+d.y); });
-
-            var area2 = d3.svg.area()
-                .interpolate("monotone")
-                .x(function (d) { return x2(d.x); })
-                .y0(height2)
-                .y1(function (d) { return y2(+d.y); });
-
-            var generateTooltipInfo = function (extentX: any, viewModel: BrushChartViewModel): TooltipDataItem[] {
-                var ySum = 0;
+            if (viewModel){
                 var data = viewModel.points;
-                for (var i = 0; i < data.length; i++) {
-                    if (extentX[0] <= data[i].x && data[i].x <= extentX[1]) {
-                        ySum = ySum + data[i].y;
+
+                // apply visual style and set functionalities
+                var x = d3.time.scale().range([0, width]),
+                    x2 = d3.time.scale().range([0, width]),
+                    y = d3.scale.linear().range([height, 0]),
+                    y2 = d3.scale.linear().range([height2, 0]);
+
+                var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+                    xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+                    yAxis = d3.svg.axis().scale(y).orient("left");
+
+                var area = d3.svg.area()
+                    .interpolate("monotone")
+                    .x(function (d) { return x(d.x); })
+                    .y0(height)
+                    .y1(function (d) { return y(+d.y); });
+
+                var area2 = d3.svg.area()
+                    .interpolate("monotone")
+                    .x(function (d) { return x2(d.x); })
+                    .y0(height2)
+                    .y1(function (d) { return y2(+d.y); });
+
+                var generateTooltipInfo = function (extentX: any, viewModel: BrushChartViewModel): TooltipDataItem[] {
+                    var ySum = 0;
+                    var data = viewModel.points;
+                    for (var i = 0; i < data.length; i++) {
+                        if (extentX[0] <= data[i].x && data[i].x <= extentX[1]) {
+                            ySum = ySum + data[i].y;
+                        }
                     }
-                }
 
-                return [
-                    {
-                        displayName: 'Time Range (' + viewModel.xLabel + ')',
-                        value: extentX[0].toDateString() + ' - ' + extentX[1].toDateString()
-                    },
-                    {
-                        displayName: 'Total (' + viewModel.yLabel + ')',
-                        value: valueFormatter.format(ySum, viewModel.yFormat)
-                    }];
-            };
+                    return [
+                        {
+                            displayName: 'Time Range (' + viewModel.xLabel + ')',
+                            value: extentX[0].toDateString() + ' - ' + extentX[1].toDateString()
+                        },
+                        {
+                            displayName: 'Total (' + viewModel.yLabel + ')',
+                            value: valueFormatter.format(ySum, viewModel.yFormat)
+                        }];
+                };
 
-            var brush = d3.svg.brush()
-                .x(x2)
-                .on("brush", function brushed() {
-                    x.domain(brush.empty() ? x2.domain() : brush.extent());
-                    focus.select(".area").empty();
-                    focus.select(".area").attr("d", area);
-                    focus.select(".x.axis").call(xAxis);
-                    // generate back the tooltip
-                    var tooltip = generateTooltipInfo(brush.extent(), viewModel);
-                    TooltipManager.addTooltip(focus, (tooltipEvent: TooltipEvent) => tooltip);
-                }, false);
+                var brush = d3.svg.brush()
+                    .x(x2)
+                    .on("brush", function brushed() {
+                        x.domain(brush.empty() ? x2.domain() : brush.extent());
+                        focus.select(".area").empty();
+                        focus.select(".area").attr("d", area);
+                        focus.select(".x.axis").call(xAxis);
+                        // generate back the tooltip
+                        var tooltip = generateTooltipInfo(brush.extent(), viewModel);
+                        TooltipManager.addTooltip(focus, (tooltipEvent: TooltipEvent) => tooltip);
+                    }, false);
 
-            this.rect
-                .attr("width", width)
-                .attr("height", height);
+                this.rect
+                    .attr("width", width)
+                    .attr("height", height);
 
-            this.axisYRect
-                .attr("width", margin.left)
-                .attr("height", height)
-                .attr("x", -1 * margin.left)
-                .attr("fill", "#fff");
+                this.axisYRect
+                    .attr("width", margin.left)
+                    .attr("height", height)
+                    .attr("x", -1 * margin.left)
+                    .attr("fill", "#fff");
 
-            this.axisYRectRight
-                .attr("width", margin.left)
-                .attr("height", height)
-                .attr("x", width)
-                .attr("fill", "#fff");
+                this.axisYRectRight
+                    .attr("width", margin.left)
+                    .attr("height", height)
+                    .attr("x", width)
+                    .attr("fill", "#fff");
 
-            var focus = this.focus;
-            focus.attr("class", "focus")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                var focus = this.focus;
+                focus.attr("class", "focus")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var context = this.context;
-            context.attr("class", "context")
-                .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+                var context = this.context;
+                context.attr("class", "context")
+                    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-            x.domain(d3.extent(data.map(function (d) { return d.x; })));
-            y.domain([0, d3.max(data.map(function (d) { return +d.y; }))]);
-            x2.domain(x.domain());
-            y2.domain(y.domain());
+                x.domain(d3.extent(data.map(function (d) { return d.x; })));
+                y.domain([0, d3.max(data.map(function (d) { return +d.y; }))]);
+                x2.domain(x.domain());
+                y2.domain(y.domain());
 
-            this.focusArea
-                .datum(data)
-                .attr("class", "area")
-                .attr("d", area)
-                .attr('fill', this.getFill(this.dataView, 'fill').solid.color)
-                .attr('fill-opacity', .5)
-                .attr('clip-path', 'url(#clip)')
-                .attr('fill-opacity', .4)
-                .attr('stroke', this.getFill(this.dataView, 'fill').solid.color)
-                .attr('stroke-width', 1.8);
+                this.focusArea
+                    .datum(data)
+                    .attr("class", "area")
+                    .attr("d", area)
+                    .attr('fill', this.getFill(this.dataView, 'fill').solid.color)
+                    .attr('fill-opacity', .5)
+                    .attr('clip-path', 'url(#clip)')
+                    .attr('fill-opacity', .4)
+                    .attr('stroke', this.getFill(this.dataView, 'fill').solid.color)
+                    .attr('stroke-width', 1.8);
 
-            this.focusX.attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
+                this.focusX.attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
 
-            this.focusY.attr("class", "y axis")
-                .call(yAxis);
+                this.focusY.attr("class", "y axis")
+                    .call(yAxis);
 
-            this.focusX.select('path')
-                .attr('fill', 'none')
-                .attr('stroke', '#000')
-                .attr('shape-rendering', 'crispEdges');
+                this.focusX.select('path')
+                    .attr('fill', 'none')
+                    .attr('stroke', '#000')
+                    .attr('shape-rendering', 'crispEdges');
 
-            this.focusX.select('line')
-                .attr('fill', 'none')
-                .attr('stroke', '#000')
-                .attr('shape-rendering', 'crispEdges');
+                this.focusX.select('line')
+                    .attr('fill', 'none')
+                    .attr('stroke', '#000')
+                    .attr('shape-rendering', 'crispEdges');
 
-            this.focusY.select('path')
-                .attr('fill', 'none')
-                .attr('stroke', '#000')
-                .attr('shape-rendering', 'crispEdges');
+                this.focusY.select('path')
+                    .attr('fill', 'none')
+                    .attr('stroke', '#000')
+                    .attr('shape-rendering', 'crispEdges');
 
-            this.focusY.select('line')
-                .attr('fill', 'none')
-                .attr('stroke', '#000')
-                .attr('shape-rendering', 'crispEdges');
+                this.focusY.select('line')
+                    .attr('fill', 'none')
+                    .attr('stroke', '#000')
+                    .attr('shape-rendering', 'crispEdges');
 
-            this.focusY.attr('background-color', '#fff');
-            this.focusY.append("rect");
+                this.focusY.attr('background-color', '#fff');
+                this.focusY.append("rect");
 
-            this.contextArea.datum(data)
-                .attr("class", "area brush")
-                .attr("d", area2)
-                .attr('fill', this.getFill(this.dataView, 'fill1').solid.color)
-                .attr('clip-path', 'url(#clip)')
-                .attr('fill-opacity', .4)
-                .attr('stroke', this.getFill(this.dataView, 'fill1').solid.color)
-                .attr('stroke-width', 1.8);
+                this.contextArea.datum(data)
+                    .attr("class", "area brush")
+                    .attr("d", area2)
+                    .attr('fill', this.getFill(this.dataView, 'fill1').solid.color)
+                    .attr('clip-path', 'url(#clip)')
+                    .attr('fill-opacity', .4)
+                    .attr('stroke', this.getFill(this.dataView, 'fill1').solid.color)
+                    .attr('stroke-width', 1.8);
 
-            this.contextX.attr("class", "x axis")
-                .attr("transform", "translate(0," + height2 + ")")
-                .call(xAxis2);
+                this.contextX.attr("class", "x axis")
+                    .attr("transform", "translate(0," + height2 + ")")
+                    .call(xAxis2);
 
-            this.contextY.attr("class", "x brush")
-                .call(brush)
-                .selectAll('rect')
-                .attr('y', -6)
-                .attr('height', height2 + 7)
-                .attr('drag-resize-disabled', true);
+                this.contextY.attr("class", "x brush")
+                    .call(brush)
+                    .selectAll('rect')
+                    .attr('y', -6)
+                    .attr('height', height2 + 7)
+                    .attr('drag-resize-disabled', true);
 
-            this.contextX.select('path')
-                .attr('fill', 'none')
-                .attr('stroke', '#000')
-                .attr('shape-rendering', 'crispEdges');
+                this.contextX.select('path')
+                    .attr('fill', 'none')
+                    .attr('stroke', '#000')
+                    .attr('shape-rendering', 'crispEdges');
 
-            this.contextX.select('line')
-                .attr('fill', 'none')
-                .attr('stroke', '#000')
-                .attr('shape-rendering', 'crispEdges');
+                this.contextX.select('line')
+                    .attr('fill', 'none')
+                    .attr('stroke', '#000')
+                    .attr('shape-rendering', 'crispEdges');
 
-            this.contextY.select('.extent')
-                .attr('stroke', '#fff')
-                .attr('fill-opacity', '.125')
-                .attr('shape-rendering', 'crispEdges');
+                this.contextY.select('.extent')
+                    .attr('stroke', '#fff')
+                    .attr('fill-opacity', '.125')
+                    .attr('shape-rendering', 'crispEdges');
 
-            TooltipManager.addTooltip(this.focusArea, (tooltipEvent: TooltipEvent) => generateTooltipInfo(x2.domain(), viewModel));
+                TooltipManager.addTooltip(this.focusArea, (tooltipEvent: TooltipEvent) => generateTooltipInfo(x2.domain(), viewModel));
+            }
         }
 
         // Define visual properties
